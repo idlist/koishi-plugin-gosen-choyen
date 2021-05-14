69be7e18e05a9e25a76b87a8ab4838caf05f1557
i'DLisT <i.dlist@outlook.com>
2021-05-14 15:21:39 +0800

    chore: v2 legacy support
    Does anyone still use Koishi v2 ...?
    
    
    Former-commit-id: 72b1e080044339c1960d66c9f1006874a91547d9

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const generateImage = require('./generate-image')
-const { s } = require('koishi-utils')
 
 class Config {
   constructor(config) {
     // default value for plugin options
-    this.version = 3
     this.asSubcommand = (config && config.asSubcommand) ?? false
     this.disableCQCode = false
     this.maxLength = 42
@@ -81,7 +79,7 @@ module.exports.apply = (ctx, config) => {
   config = new Config(config)
   let logger = ctx.logger('gosen-choyen')
 
-  let thisCommand = ctx
+  let gosenCommand = ctx
     .command('5k <upper> <lower>', '生成5000兆円风格字体')
     .usage('若upper或lower为""可使其为空；含有空格或以-开头的内容需要用""包围起来。')
     .option('offset', '-x <px> 设置第二行偏移量（默认为200px）')
@@ -89,73 +87,32 @@ module.exports.apply = (ctx, config) => {
     .example('5k 5000兆円 欲しい！  生成字体图')
 
   // version
-  switch (config.version) {
-    case 2:
-      thisCommand.action(async ({ session, options }, upper, lower) => {
-        // initialize options
-        options = new Options(options, config)
+  gosenCommand.action(async ({ session, options }, upper, lower) => {
+    // initialize options
+    options = new Options(options, config)
 
-        // manipulate content
-        const content = new Content(options, upper, lower)
-        if (content.errCode) {
-          logger.debug('arguments or options are incorrect')
-          switch (content.errCode) {
-            case 1:
-              session.$send('没识别到内容。')
-              return
-            case 2:
-              session.$send('内容太长了。')
-              return
-          }
-        }
+    // manipulate content
+    const content = new Content(options, upper, lower)
+    if (content.errCode) {
+      logger.debug('arguments or options are incorrect')
+      switch (content.errCode) {
+        case 1:
+          session.$send('没识别到内容。')
+          return
+        case 2:
+          session.$send('内容太长了。')
+          return
+      }
+    }
 
-        // generate and send image
-        const canvas = generateImage(options, content.upper, content.lower)
-        try {
-          const imageData = canvas.toBuffer().toString('base64')
-          session.$send(`[CQ:image,file=base64://${imageData}]`)
-        } catch (err) {
-          logger.warn('something went wrong when sending image')
-          console.log(err)
-        }
-      })
-      break
-    case 3:
-      thisCommand.action(async ({ options }, upper, lower) => {
-        // initialize options
-        options = new Options(options, config)
-
-        // manipulate content
-        const content = new Content(options, upper, lower)
-        if (content.errCode) {
-          logger.debug('arguments or options were incorrect')
-          switch (content.errCode) {
-            case 1:
-              return '没识别到内容。'
-            case 2:
-              return '内容太长了。'
-          }
-        }
-
-        // generate and send image
-        const canvas = generateImage(options, content.upper, content.lower)
-        try {
-          const imageData = canvas.toBuffer().toString('base64')
-          return s('image', { url: `base64://${imageData}` })
-        } catch (err) {
-          logger.warn('something went wrong when sending image')
-          console.log(err)
-        }
-      })
-      break
-    default:
-      logger.error('version is not correct or supported, gosen-choyen is disposed')
-      ctx.command('5k').dispose()
-      break
-  }
-
-  // asSubcommand
-  if (config.version && config.asSubcommand) {
-    ctx.command(config.asSubcommand).subcommand('5k')
-  }
+    // generate and send image
+    const canvas = generateImage(options, content.upper, content.lower)
+    try {
+      const imageData = canvas.toBuffer().toString('base64')
+      session.$send(`[CQ:image,file=base64://${imageData}]`)
+    } catch (err) {
+      logger.warn('something went wrong when sending image')
+      console.log(err)
+    }
+  })
 }
